30f7cf3cce9739cc742c8c5ae8f7736c56846952
Brandon Weber <brandon@basistheory.com>
2021-05-24 14:38:49 -0500

    chore: add token types

---
+++ src/BasisTheory.net/Tokens/Constants/TokenTypes.cs
@@ -0,0 +1,11 @@
+namespace BasisTheory.net.Tokens.Constants
+{
+    public class TokenTypes
+    {
+        public const string Card = "card";
+        public const string Bank = "bank";
+        public const string Token = "token";
+        public const string Mask = "mask";
+        public const string BillingDetails = "billing_details";
+    }
+}
