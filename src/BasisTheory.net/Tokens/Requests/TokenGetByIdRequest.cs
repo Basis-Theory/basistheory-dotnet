4b1c0308e34ad204dc122449feda8652ca3d8f8f
Brandon Weber <brandon@basistheory.com>
2021-05-27 07:32:05 -0500

    chore: don't require children parameter

--- src/BasisTheory.net/Tokens/Requests/TokenGetByIdRequest.cs
+++ src/BasisTheory.net/Tokens/Requests/TokenGetByIdRequest.cs
@@ -6,16 +6,18 @@ namespace BasisTheory.net.Tokens.Requests
 {
     public class TokenGetByIdRequest : GetRequest
     {
-        public bool Children { get; set; }
+        public bool? Children { get; set; }
         public List<string> ChildrenTypes { get; set; } = new List<string>();
 
         public override string BuildQuery()
         {
             var queryParts = new List<string>();
-            if (!Children) return null;
 
-            queryParts.Add("children=true");
-            queryParts.AddRange(ChildrenTypes.Select(childrenType => $"children_type={childrenType}"));
+            if (Children.HasValue)
+                queryParts.Add($"children={Children.Value.ToString().ToLower()}");
+
+            if (ChildrenTypes.Any())
+                queryParts.AddRange(ChildrenTypes.Select(childrenType => $"children_type={childrenType}"));
 
             return string.Join("&", queryParts);
         }
--- src/BasisTheory.net/Tokens/Requests/TokenGetRequest.cs
+++ src/BasisTheory.net/Tokens/Requests/TokenGetRequest.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using BasisTheory.net.Common.Requests;
@@ -8,7 +9,7 @@ namespace BasisTheory.net.Tokens.Requests
     {
         public List<string> Types { get; set; } = new List<string>();
 
-        public bool Children { get; set; }
+        public bool? Children { get; set; }
         public List<string> ChildrenTypes { get; set; } = new List<string>();
 
         public override string BuildQuery()
@@ -23,11 +24,11 @@ namespace BasisTheory.net.Tokens.Requests
 
             queryParts.AddRange(Types.Select(type => $"type={type}"));
 
-            if (Children)
-            {
-                queryParts.Add("children=true");
+            if (Children.HasValue)
+                queryParts.Add($"children={Children.Value.ToString().ToLower()}");
+
+            if (ChildrenTypes.Any())
                 queryParts.AddRange(ChildrenTypes.Select(childrenType => $"children_type={childrenType}"));
-            }
 
             return string.Join("&", queryParts);
         }
