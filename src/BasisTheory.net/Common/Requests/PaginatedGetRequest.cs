c1ab2d5238aa41aed7bc332c47e8ad60374aa2ae
Adam Letizia <adam@basistheory.com>
2021-07-14 15:18:36 -0500

    chore: Adds FromQuery attributes to Pagination query params to fix proxying requests

--- src/BasisTheory.net/Common/Requests/PaginatedGetRequest.cs
+++ src/BasisTheory.net/Common/Requests/PaginatedGetRequest.cs
@@ -1,8 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+
 namespace BasisTheory.net.Common.Requests
 {
     public abstract class PaginatedGetRequest : GetRequest
     {
+        [FromQuery(Name = "page")]
         public int? Page { get; set; }
+
+        [FromQuery(Name = "size")]
         public int? PageSize { get; set; }
     }
 }
