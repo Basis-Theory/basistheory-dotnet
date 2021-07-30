805448821857841c71ec711c591410edbff25d80
Brandon Weber <brandon@basistheory.com>
2021-07-30 07:10:13 -0500

    feat: rename token types

--- src/BasisTheory.net/Tokens/Constants/TokenTypes.cs
+++ src/BasisTheory.net/Tokens/Constants/TokenTypes.cs
@@ -2,12 +2,12 @@ namespace BasisTheory.net.Tokens.Constants
 {
     public class TokenTypes
     {
+        public const string Token = "token";
         public const string Card = "card";
         public const string Bank = "bank";
-        public const string Token = "token";
         public const string Mask = "mask";
         public const string BillingDetails = "billing_details";
-        public const string CardExchanged = "card:exchanged";
-        public const string BankExchanged = "bank:exchanged";
+        public const string CardReaction = "card:reaction";
+        public const string BankReaction = "bank:reaction";
     }
 }
