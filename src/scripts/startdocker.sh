2e935641324f398313df5b88b6761a3a74215a9a
Brandon Weber <brandon@basistheory.com>
2021-08-02 16:00:16 -0500

    chore: Trust self signed cert if on Linux

--- src/scripts/startdocker.sh
+++ src/scripts/startdocker.sh
@@ -5,6 +5,11 @@ current_directory="$PWD"
 
 cd $(dirname $0)/..
 
+if [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+    cp local-certs/azure-key-vault-emulator.crt /usr/local/share/ca-certificates/azure-key-vault-emulator.crt
+    update-ca-certificates
+fi
+
 docker-compose up -d --build
 
 result=$?
