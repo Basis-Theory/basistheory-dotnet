ffcb4bf1ddcd0a0a76099957787faaa353de3dd7
Brandon Weber <brandon@basistheory.com>
2021-08-02 16:32:39 -0500

    chore: ensure certificate is imported to run tests

--- src/scripts/startdocker.sh
+++ src/scripts/startdocker.sh
@@ -5,11 +5,6 @@ current_directory="$PWD"
 
 cd $(dirname $0)/..
 
-if [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
-    sudo cp local-certs/azure-key-vault-emulator.crt /usr/local/share/ca-certificates/azure-key-vault-emulator.crt
-    sudo update-ca-certificates
-fi
-
 docker-compose up -d --build
 
 result=$?
