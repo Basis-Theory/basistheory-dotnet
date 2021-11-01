cf4258f99ff35d87472d85e083e98ec89383861b
Brandon Weber <brandon@basistheory.com>
2021-11-01 17:02:06 -0500

    feat: remove enrichment properties from tenant usage report (#79)

--- src/BasisTheory.net/Tenants/Entities/TenantUsageReport.cs
+++ src/BasisTheory.net/Tenants/Entities/TenantUsageReport.cs
@@ -14,22 +14,6 @@ namespace BasisTheory.net.Tenants.Entities
 
     public class TokenReport
     {
-        [JsonProperty("enrichment_limit")]
-        [JsonPropertyName("enrichment_limit")]
-        public long? EnrichmentLimit { get; set; }
-
-        [JsonProperty("free_enriched_token_limit")]
-        [JsonPropertyName("free_enriched_token_limit")]
-        public long? FreeEnrichedTokenLimit { get; set; }
-
-        [JsonProperty("number_of_enriched_tokens")]
-        [JsonPropertyName("number_of_enriched_tokens")]
-        public long NumberOfEnrichedTokens { get; set; }
-
-        [JsonProperty("number_of_enrichments")]
-        [JsonPropertyName("number_of_enrichments")]
-        public long NumberOfEnrichments { get; set; }
-
         [JsonProperty("metrics_by_type")]
         [JsonPropertyName("metrics_by_type")]
         public Dictionary<string, TokenTypeMetrics> TokenTypeMetrics { get; set; }
