ba24ccd705ecf84bbb764aef7eefcdb89fd5e47c
Brandon Weber <brandon@basistheory.com>
2021-11-04 16:06:02 -0500

    feat: remove unused token types

--- src/BasisTheory.net/Tokens/Constants/TokenTypes.cs
+++ src/BasisTheory.net/Tokens/Constants/TokenTypes.cs
@@ -5,7 +5,5 @@ namespace BasisTheory.net.Tokens.Constants
         public const string Token = "token";
         public const string Card = "card";
         public const string Bank = "bank";
-        public const string CardReaction = "card:reaction";
-        public const string BankReaction = "bank:reaction";
     }
 }
