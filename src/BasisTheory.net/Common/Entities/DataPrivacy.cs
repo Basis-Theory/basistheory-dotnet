c6cfaa317964807bce272a1689669b954acc45bc
Adam Letizia <adam@basistheory.com>
2021-12-15 12:49:14 -0600

    fix: add JsonPropertyOrder to data privacy for System.Text.Json serialization

--- src/BasisTheory.net/Common/Entities/DataPrivacy.cs
+++ src/BasisTheory.net/Common/Entities/DataPrivacy.cs
@@ -6,15 +6,15 @@ namespace BasisTheory.net.Common.Entities
     public class DataPrivacy
     {
         [JsonProperty("classification", Order = 0)]
-        [JsonPropertyName("classification")]
+        [JsonPropertyName("classification"), JsonPropertyOrder(0)]
         public string Classification { get; set; }
 
         [JsonProperty("impact_level", Order = 1)]
-        [JsonPropertyName("impact_level")]
+        [JsonPropertyName("impact_level"), JsonPropertyOrder(1)]
         public string ImpactLevel { get; set; }
 
         [JsonProperty("restriction_policy", Order = 2)]
-        [JsonPropertyName("restriction_policy")]
+        [JsonPropertyName("restriction_policy"), JsonPropertyOrder(2)]
         public string RestrictionPolicy { get; set; }
     }
 }
