20593fc251ce388892072033e22b8a6687c2b85a
Adam Letizia <adam@basistheory.com>
2021-12-15 12:51:41 -0600

    fix: add JsonPropertyOrder attributes to TokenResponse for System.Text.Json serialization

--- src/BasisTheory.net/Common/Responses/TokenResponse.cs
+++ src/BasisTheory.net/Common/Responses/TokenResponse.cs
@@ -8,39 +8,39 @@ namespace BasisTheory.net.Common.Responses
     public abstract class TokenResponse
     {
         [JsonProperty("id", Order = -3)]
-        [JsonPropertyName("id")]
+        [JsonPropertyName("id"), JsonPropertyOrder(-3)]
         public Guid Id { get; set; }
 
         [JsonProperty("tenant_id", Order = -2)]
-        [JsonPropertyName("tenant_id")]
+        [JsonPropertyName("tenant_id"), JsonPropertyOrder(-2)]
         public Guid? TenantId { get; set; }
 
         [JsonProperty("type", Order = -1)]
-        [JsonPropertyName("type")]
+        [JsonPropertyName("type"), JsonPropertyOrder(-1)]
         public string Type { get; set; }
 
         [JsonProperty("fingerprint", Order = 0)]
-        [JsonPropertyName("fingerprint")]
+        [JsonPropertyName("fingerprint"), JsonPropertyOrder(0)]
         public string Fingerprint { get; set; }
 
         [JsonProperty("metadata", Order = 1)]
-        [JsonPropertyName("metadata")]
+        [JsonPropertyName("metadata"), JsonPropertyOrder(1)]
         public Dictionary<string, string> Metadata { get; set; }
 
         [JsonProperty("created_by", Order = 2)]
-        [JsonPropertyName("created_by")]
+        [JsonPropertyName("created_by"), JsonPropertyOrder(2)]
         public Guid? CreatedBy { get; set; }
 
         [JsonProperty("created_at", Order = 3)]
-        [JsonPropertyName("created_at")]
+        [JsonPropertyName("created_at"), JsonPropertyOrder(3)]
         public DateTimeOffset? CreatedDate { get; set; }
 
         [JsonProperty("modified_by", Order = 4)]
-        [JsonPropertyName("modified_by")]
+        [JsonPropertyName("modified_by"), JsonPropertyOrder(4)]
         public Guid? ModifiedBy { get; set; }
 
         [JsonProperty("modified_at", Order = 5)]
-        [JsonPropertyName("modified_at")]
+        [JsonPropertyName("modified_at"), JsonPropertyOrder(5)]
         public DateTimeOffset? ModifiedDate { get; set; }
     }
 }
