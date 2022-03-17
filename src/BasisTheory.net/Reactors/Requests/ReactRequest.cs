845dfd63fb23745134dc3379decee68582175e56
Josue <jolesa97@gmail.com>
2022-03-17 10:21:49 -0600

    feat: adding json property

--- src/BasisTheory.net/Reactors/Requests/ReactRequest.cs
+++ src/BasisTheory.net/Reactors/Requests/ReactRequest.cs
@@ -1,3 +1,4 @@
+using System.Text.Json.Serialization;
 using Newtonsoft.Json;
 
 namespace BasisTheory.net.Reactors.Requests
@@ -5,6 +6,7 @@ namespace BasisTheory.net.Reactors.Requests
     public class ReactRequest
     {
         [JsonProperty("args")]
+        [JsonPropertyName("args")]
         public dynamic Arguments { get; set; }
     }
 }
