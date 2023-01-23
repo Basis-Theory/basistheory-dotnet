c265c00d7b895a213fc3653960453dfb32d349ba
Drew Hudec <drew@basistheory.com>
2023-01-23 13:01:01 -0500

    fix: Fixes namespace on CreateSessionResponse (#178)

--- src/BasisTheory.net/Sessions/SessionClient.cs
+++ src/BasisTheory.net/Sessions/SessionClient.cs
@@ -4,6 +4,7 @@ using System.Threading.Tasks;
 using BasisTheory.net.Common;
 using BasisTheory.net.Common.Entities;
 using BasisTheory.net.Common.Requests;
+using BasisTheory.net.Sessions.Responses;
 using BasisTheory.net.Sessions.Requests;
 using BasisTheory.net.Tokens.Entities;
 
