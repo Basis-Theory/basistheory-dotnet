e30bf35cad69f307c895fe560a0dc2e089365e07
Amber Torres <amber_kerawala@berkeley.edu>
2023-01-23 11:08:45 -0500

    feat: fix namespace in AuthorizeSessionRequest.cs

--- src/BasisTheory.net/Sessions/Requests/AuthorizeSessionRequest.cs
+++ src/BasisTheory.net/Sessions/Requests/AuthorizeSessionRequest.cs
@@ -4,22 +4,25 @@ using System.Text.Json.Serialization;
 using BasisTheory.net.Applications.Entities;
 using Newtonsoft.Json;
 
-public class AuthorizeSessionRequest
+namespace BasisTheory.net.Sessions.Requests
 {
-    [JsonProperty("nonce")]
-    [JsonPropertyName("nonce")]
-    public string Nonce { get; set; }
+    public class AuthorizeSessionRequest
+    {
+        [JsonProperty("nonce")]
+        [JsonPropertyName("nonce")]
+        public string Nonce { get; set; }
 
-    [JsonProperty("expires_at")]
-    [JsonPropertyName("expires_at")]
-    public DateTimeOffset? ExpiresAt { get; set; }
+        [JsonProperty("expires_at")]
+        [JsonPropertyName("expires_at")]
+        public DateTimeOffset? ExpiresAt { get; set; }
 #nullable enable
-    [JsonProperty("permissions")]
-    [JsonPropertyName("permissions")]
-    public List<string>? Permissions { get; set; }
+        [JsonProperty("permissions")]
+        [JsonPropertyName("permissions")]
+        public List<string>? Permissions { get; set; }
         
-    [JsonProperty("rules")]
-    [JsonPropertyName("rules")]
-    public List<AccessRule>? Rules { get; set; }
+        [JsonProperty("rules")]
+        [JsonPropertyName("rules")]
+        public List<AccessRule>? Rules { get; set; }
 #nullable disable
+    }
 }
