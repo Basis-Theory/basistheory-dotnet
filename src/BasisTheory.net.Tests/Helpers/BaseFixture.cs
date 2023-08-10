809e8af3651697dd772f0fca7842f794676681eb
Drew Hudec <drew@basistheory.com>
2023-08-10 09:41:39 -0400

    chore: Replaces Moq with NSubstitute

--- src/BasisTheory.net.Tests/Helpers/BaseFixture.cs
+++ src/BasisTheory.net.Tests/Helpers/BaseFixture.cs
@@ -2,13 +2,13 @@ using System;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Reflection;
 using System.Threading;
 using System.Threading.Tasks;
 using BasisTheory.net.Common.Entities;
 using BasisTheory.net.Common.Utilities;
 using Bogus;
-using Moq;
-using Moq.Protected;
+using NSubstitute;
 using Xunit;
 
 namespace BasisTheory.net.Tests.Helpers
@@ -17,7 +17,7 @@ namespace BasisTheory.net.Tests.Helpers
     {
         public readonly Faker Faker;
         public readonly string ApiKey;
-        private readonly Mock<HttpMessageHandler> _messageHandler;
+        private readonly HttpMessageHandler _messageHandler;
         protected readonly HttpClient HttpClient;
         protected readonly ApplicationInfo AppInfo;
 
@@ -25,8 +25,8 @@ namespace BasisTheory.net.Tests.Helpers
         {
             Faker = new Faker();
             ApiKey = Guid.NewGuid().ToString();
-            _messageHandler = new Mock<HttpMessageHandler>();
-            HttpClient = new HttpClient(_messageHandler.Object)
+            _messageHandler = Substitute.For<HttpMessageHandler>();
+            HttpClient = new HttpClient(_messageHandler)
             {
                 BaseAddress = new Uri("http://localhost/")
             };
@@ -46,27 +46,44 @@ namespace BasisTheory.net.Tests.Helpers
         public void SetupHandler(HttpStatusCode statusCode, string content = null,
             Action<HttpRequestMessage, CancellationToken> messageHandler = null)
         {
-            var httpResponseMessage = string.IsNullOrEmpty(content) ?
-                new HttpResponseMessage { StatusCode = statusCode } :
-                new HttpResponseMessage
+            var httpResponseMessage = string.IsNullOrEmpty(content)
+                ? new HttpResponseMessage { StatusCode = statusCode }
+                : new HttpResponseMessage
                 {
                     StatusCode = statusCode,
                     Content = new StringContent(content)
                 };
 
-            var returnResult = _messageHandler.Protected()
-                .Setup<Task<HttpResponseMessage>>("SendAsync", ItExpr.IsAny<HttpRequestMessage>(),
-                    ItExpr.IsAny<CancellationToken>())
-                .ReturnsAsync(httpResponseMessage);
+            var returnResult = _messageHandler
+                .Protected("SendAsync", Arg.Any<HttpRequestMessage>(),
+                    Arg.Any<CancellationToken>())
+                .Returns(Task.FromResult(httpResponseMessage));
 
             if (messageHandler != null)
-                returnResult.Callback(messageHandler);
+                returnResult.AndDoes(info => messageHandler.Invoke(
+                    info.Arg<HttpRequestMessage>(),
+                    info.Arg<CancellationToken>()
+                ));
         }
 
         public void AssertUserAgent(HttpRequestMessage requestMessage)
         {
-            Assert.Equal(UserAgentUtility.BuildUserAgentString(AppInfo), string.Join(" ", requestMessage.Headers.GetValues("User-Agent")));
-            Assert.Equal(UserAgentUtility.BuildBtClientUserAgentString(AppInfo), requestMessage.Headers.GetValues("BT-CLIENT-USER-AGENT").First());
+            Assert.Equal(UserAgentUtility.BuildUserAgentString(AppInfo),
+                string.Join(" ", requestMessage.Headers.GetValues("User-Agent")));
+            Assert.Equal(UserAgentUtility.BuildBtClientUserAgentString(AppInfo),
+                requestMessage.Headers.GetValues("BT-CLIENT-USER-AGENT").First());
+        }
+    }
+
+    public static class Reflect
+    {
+        public static object Protected(this object target, string name, params object[] args)
+        {
+            var type = target.GetType();
+            var method = type.GetMethods(BindingFlags.NonPublic | BindingFlags.Instance)
+                .Single(x => x.Name == name);
+            
+            return method.Invoke(target, args);
         }
     }
 }
