b2fae9ff644b8ed2a7b7be58f9121e3ed98e92df
Drew Hudec <drew@basistheory.com>
2023-08-25 16:50:58 -0400

    Adds faker enrichment model

--- src/BasisTheory.net.Tests/Tokens/Helpers/TokenFactory.cs
+++ src/BasisTheory.net.Tests/Tokens/Helpers/TokenFactory.cs
@@ -33,7 +33,8 @@ public static class TokenFactory
         .RuleFor(t => t.Privacy, _ => DataPrivacyFaker.Generate())
         .RuleFor(t => t.Containers, f => new List<string> { $"/{f.Lorem.Word()}/{f.Lorem.Word()}/" })
         .RuleFor(t => t.SearchIndexes, (f, _) => f.Make(f.Random.Int(1, 5), () => f.Random.String2(10)))
-        .RuleFor(t => t.ExpiresAt, _ => DateTimeOffset.Now.AddDays(1));
+        .RuleFor(t => t.ExpiresAt, _ => DateTimeOffset.Now.AddDays(1))
+        .RuleFor(t => t.Enrichments, _ => TokenEnrichmentsFaker.Generate());
 
     public static readonly Faker<TokenCreateRequest> TokenCreateRequestFaker = new Faker<TokenCreateRequest>()
         .RuleFor(t => t.Id, _ => Guid.NewGuid().ToString())
@@ -111,39 +112,90 @@ public static class TokenFactory
                 DataRestrictionPolicy.MASK,
                 DataRestrictionPolicy.REDACT));
 
+    public static readonly Faker<TokenEnrichments> TokenEnrichmentsFaker = new Faker<TokenEnrichments>()
+        .RuleFor(t => t.BinDetails, (_, _) => BinDetailsFaker.Generate());
+
+    public static readonly Faker<BinDetails> BinDetailsFaker = new Faker<BinDetails>()
+        .RuleFor(r => r.Bank, (f, _) => new BinDetails.BankModel
+        {
+            Name = f.Company.CompanyName(),
+            Phone = f.Phone.PhoneNumber(),
+            Url = f.Internet.Url(),
+            CleanName = f.Company.CompanyName()
+        })
+        .RuleFor(r => r.Country, (f, _) => new BinDetails.CountryModel
+        {
+            Name = f.Address.Country(),
+            Alpha2 = f.Address.CountryCode(),
+            Numeric = f.Random.Int(1, 999).ToString().PadLeft(3, '0')
+        })
+        .RuleFor(r => r.Product, (f, _) => new BinDetails.ProductModel
+        {
+            Name = Guid.NewGuid().ToString(),
+            Code = f.PickRandom(null, "F", "G", "A")
+        })
+        .RuleFor(r => r.CardBrand, f => f.PickRandom("VISA", "MASTERCARD", "AMEX"))
+        .RuleFor(r => r.Type, f => f.PickRandom("Credit", "Debit"))
+        .RuleFor(r => r.Prepaid, f => f.Random.Bool())
+        .RuleFor(r => r.Reloadable, f => f.Random.Bool())
+        .RuleFor(r => r.PanOrToken, f => f.PickRandom("pan", "token"))
+        .RuleFor(r => r.AccountUpdater, f => f.Random.Bool())
+        .RuleFor(r => r.AccountLevelManagement, f => f.Random.Bool())
+        .RuleFor(r => r.DomesticOnly, f => f.Random.Bool())
+        .RuleFor(r => r.GamblingBlocked, f => f.Random.Bool())
+        .RuleFor(r => r.Level2, f => f.Random.Bool())
+        .RuleFor(r => r.Level3, f => f.Random.Bool())
+        .RuleFor(r => r.IssuerCurrency, f => f.PickRandom(null, "USD", "EUR", "BRL"))
+        .RuleFor(r => r.CardSegmentType, f => f.PickRandom(null, "Consumer", "Business", "Commercial"))
+        .RuleFor(r => r.ComboCard, f => f.PickRandom(null, "Credit and Prepaid", "Credit and Debit"))
+        .RuleFor(r => r.BinLength, f => f.PickRandom(6, 8))
+        .RuleFor(r => r.Authentication, f => f.PickRandom(
+            Array.Empty<object>(),
+            new object[]
+            {
+                new
+                {
+                    sca_name = "EU PSD2 - SCA"
+                }
+            }))
+        .RuleFor(r => r.Cost,
+            f => f.PickRandom(
+                Array.Empty<object>(),
+                new object[]
+                {
+                    new
+                    {
+                        cap_fixed_amount = 0.21,
+                        cap_advalorem_amount = 0.0005,
+                        cap_type_qualifier_text = "US DURBIN REGULATION DEBIT VISA"
+                    }
+                }));
+
     public static Token Token(Action<Token> applyOverrides = null)
     {
         var token = TokenFaker.Generate();
-
         applyOverrides?.Invoke(token);
-
         return token;
     }
 
     public static TokenCreateRequest TokenCreateRequest(Action<TokenCreateRequest> applyOverrides = null)
     {
         var request = TokenCreateRequestFaker.Generate();
-
         applyOverrides?.Invoke(request);
-
         return request;
     }
 
     public static TokenUpdateRequest TokenUpdateRequest(Action<TokenUpdateRequest> applyOverrides = null)
     {
         var request = TokenUpdateRequestFaker.Generate();
-
         applyOverrides?.Invoke(request);
-
         return request;
     }
 
     public static PaginatedList<Token> PaginatedTokens(Action<PaginatedList<Token>> applyOverrides = null)
     {
         var list = PaginatedListFaker.Generate();
-
         applyOverrides?.Invoke(list);
-
         return list;
     }
 }
