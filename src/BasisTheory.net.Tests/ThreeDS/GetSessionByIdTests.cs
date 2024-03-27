c4a8200c6c044acb41dde288e0a45e33df9e0ca5
Lucas Chociay <lucas@basistheory.com>
2024-03-27 11:27:59 -0300

    test: fix tests

--- src/BasisTheory.net.Tests/ThreeDS/GetSessionByIdTests.cs
+++ src/BasisTheory.net.Tests/ThreeDS/GetSessionByIdTests.cs
@@ -63,53 +63,53 @@ namespace BasisTheory.net.Tests.ThreeDS
       _fixture.AssertUserAgent(requestMessage);
     }
 
-            [Theory]
-        [MemberData(nameof(Methods))]
-        public async Task ShouldBubbleUpBasisTheoryErrors(
-            Func<IThreeDSClient, Guid, RequestOptions, Task<ThreeDSSession>> mut)
-        {
-            var error = BasisTheoryErrorFactory.BasisTheoryError();
-            var expectedSerializedError = JsonConvert.SerializeObject(error);
+    [Theory]
+    [MemberData(nameof(Methods))]
+    public async Task ShouldBubbleUpBasisTheoryErrors(
+    Func<IThreeDSClient, string, RequestOptions, Task<ThreeDSSession>> mut)
+    {
+      var error = BasisTheoryErrorFactory.BasisTheoryError();
+      var expectedSerializedError = JsonConvert.SerializeObject(error);
 
-            _fixture.SetupHandler(HttpStatusCode.BadRequest, expectedSerializedError);
+      _fixture.SetupHandler(HttpStatusCode.BadRequest, expectedSerializedError);
 
-            var exception =
-                await Assert.ThrowsAsync<BasisTheoryException>(() => mut(_fixture.Client, Guid.NewGuid(), null));
-            var actualSerializedError = JsonConvert.SerializeObject(exception.Error);
+      var exception =
+          await Assert.ThrowsAsync<BasisTheoryException>(() => mut(_fixture.Client, Guid.NewGuid().ToString(), null));
+      var actualSerializedError = JsonConvert.SerializeObject(exception.Error);
 
-            Assert.Equal(expectedSerializedError, actualSerializedError);
-        }
+      Assert.Equal(expectedSerializedError, actualSerializedError);
+    }
 
-        [Theory]
-        [MemberData(nameof(Methods))]
-        public async Task ShouldHandleEmptyErrorResponse(
-            Func<IThreeDSClient, Guid, RequestOptions, Task<ThreeDSSession>> mut)
-        {
-            _fixture.SetupHandler(HttpStatusCode.Forbidden);
+    [Theory]
+    [MemberData(nameof(Methods))]
+    public async Task ShouldHandleEmptyErrorResponse(
+        Func<IThreeDSClient, string, RequestOptions, Task<ThreeDSSession>> mut)
+    {
+      _fixture.SetupHandler(HttpStatusCode.Forbidden);
 
-            var exception =
-                await Assert.ThrowsAsync<BasisTheoryException>(() => mut(_fixture.Client, Guid.NewGuid(), null));
+      var exception =
+          await Assert.ThrowsAsync<BasisTheoryException>(() => mut(_fixture.Client, Guid.NewGuid().ToString(), null));
 
-            Assert.Equal(403, exception.Error.Status);
-            Assert.Null(exception.Error.Title);
-            Assert.Null(exception.Error.Detail);
-        }
+      Assert.Equal(403, exception.Error.Status);
+      Assert.Null(exception.Error.Title);
+      Assert.Null(exception.Error.Detail);
+    }
 
-        [Theory]
-        [MemberData(nameof(Methods))]
-        public async Task ShouldHandleNonBasisTheoryErrorResponse(
-            Func<IThreeDSClient, Guid, RequestOptions, Task<ThreeDSSession>> mut)
-        {
-            var error = Guid.NewGuid().ToString();
+    [Theory]
+    [MemberData(nameof(Methods))]
+    public async Task ShouldHandleNonBasisTheoryErrorResponse(
+        Func<IThreeDSClient, string, RequestOptions, Task<ThreeDSSession>> mut)
+    {
+      var error = Guid.NewGuid().ToString();
 
-            _fixture.SetupHandler(HttpStatusCode.InternalServerError, error);
+      _fixture.SetupHandler(HttpStatusCode.InternalServerError, error);
 
-            var exception =
-                await Assert.ThrowsAsync<BasisTheoryException>(() => mut(_fixture.Client, Guid.NewGuid(), null));
+      var exception =
+          await Assert.ThrowsAsync<BasisTheoryException>(() => mut(_fixture.Client, Guid.NewGuid().ToString(), null));
 
-            Assert.Equal(500, exception.Error.Status);
-            Assert.Null(exception.Error.Title);
-            Assert.Null(exception.Error.Detail);
-        }
+      Assert.Equal(500, exception.Error.Status);
+      Assert.Null(exception.Error.Title);
+      Assert.Null(exception.Error.Detail);
+    }
   }
 }
