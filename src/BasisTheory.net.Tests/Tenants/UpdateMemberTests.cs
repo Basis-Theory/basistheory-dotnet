3d91770f995bdd9fb441b6bd57919d5d4e8a9380
Robert G. Greathouse <robert@basistheory.com>
2024-06-25 13:51:34 -0500

    dotnet format

--- src/BasisTheory.net.Tests/Tenants/UpdateMemberTests.cs
+++ src/BasisTheory.net.Tests/Tenants/UpdateMemberTests.cs
@@ -4,10 +4,12 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
+using BasisTheory.net.Common.Errors;
 using BasisTheory.net.Common.Requests;
 using BasisTheory.net.Tenants;
 using BasisTheory.net.Tenants.Entities;
 using BasisTheory.net.Tenants.Requests;
+using BasisTheory.net.Tests.Helpers;
 using BasisTheory.net.Tests.Tenants.Helpers;
 using Newtonsoft.Json;
 using Xunit;
@@ -76,4 +78,109 @@ public class UpdateMemberTests: IClassFixture<TenantFixture>
         Assert.Equal(_fixture.ApiKey, requestMessage.Headers.GetValues("BT-API-KEY").First());
         _fixture.AssertUserAgent(requestMessage);
     }
+    
+    [Theory]
+        [MemberData(nameof(Methods))]
+        public async Task ShouldUpdateWithPerRequestApiKey(Func<ITenantClient, TenantUpdateRequest, RequestOptions, Task<Tenant>> mut)
+        {
+            var expectedApiKey = Guid.NewGuid().ToString();
+
+            var content = TenantFactory.Tenant();
+            var expectedSerialized = JsonConvert.SerializeObject(content);
+            var updateRequest = new TenantUpdateRequest
+            {
+                Name = content.Name,
+                Settings = content.Settings
+            };
+
+            HttpRequestMessage requestMessage = null;
+            _fixture.SetupHandler(HttpStatusCode.OK, expectedSerialized, (message, _) => requestMessage = message);
+
+            var response = await mut(_fixture.Client, updateRequest, new RequestOptions
+            {
+                ApiKey = expectedApiKey
+            });
+
+            Assert.Equal(expectedSerialized, JsonConvert.SerializeObject(response));
+            Assert.Equal(HttpMethod.Put, requestMessage.Method);
+            Assert.Equal("/tenants/self", requestMessage.RequestUri?.PathAndQuery);
+            Assert.Equal(expectedApiKey, requestMessage.Headers.GetValues("BT-API-KEY").First());
+            _fixture.AssertUserAgent(requestMessage);
+        }
+
+        [Theory]
+        [MemberData(nameof(Methods))]
+        public async Task ShouldUpdateWithCustomHeaders(Func<ITenantClient, TenantUpdateRequest, RequestOptions, Task<Tenant>> mut)
+        {
+            var expectedCorrelationId = Guid.NewGuid().ToString();
+            var expectedIdempotencyKey = Guid.NewGuid().ToString();
+
+            var content = TenantFactory.Tenant();
+            var expectedSerialized = JsonConvert.SerializeObject(content);
+            var updateRequest = new TenantUpdateRequest
+            {
+                Name = content.Name,
+                Settings = content.Settings
+            };
+
+            HttpRequestMessage requestMessage = null;
+            _fixture.SetupHandler(HttpStatusCode.OK, expectedSerialized, (message, _) => requestMessage = message);
+
+            var response = await mut(_fixture.Client, updateRequest, new RequestOptions
+            {
+                CorrelationId = expectedCorrelationId,
+                IdempotencyKey = expectedIdempotencyKey
+            });
+
+            Assert.Equal(expectedSerialized, JsonConvert.SerializeObject(response));
+            Assert.Equal(HttpMethod.Put, requestMessage.Method);
+            Assert.Equal("/tenants/self", requestMessage.RequestUri?.PathAndQuery);
+            Assert.Equal(_fixture.ApiKey, requestMessage.Headers.GetValues("BT-API-KEY").First());
+            Assert.Equal(expectedCorrelationId, requestMessage.Headers.GetValues("BT-TRACE-ID").First());
+            Assert.Equal(expectedIdempotencyKey, requestMessage.Headers.GetValues("BT-IDEMPOTENCY-KEY").First());
+            _fixture.AssertUserAgent(requestMessage);
+        }
+
+        [Theory]
+        [MemberData(nameof(Methods))]
+        public async Task ShouldBubbleUpBasisTheoryErrors(Func<ITenantClient, TenantUpdateRequest, RequestOptions, Task<Tenant>> mut)
+        {
+            var error = BasisTheoryErrorFactory.BasisTheoryError();
+            var expectedSerializedError = JsonConvert.SerializeObject(error);
+
+            _fixture.SetupHandler(HttpStatusCode.BadRequest, expectedSerializedError);
+
+            var exception = await Assert.ThrowsAsync<BasisTheoryException>(() => mut(_fixture.Client, new TenantUpdateRequest(), null));
+            var actualSerializedError = JsonConvert.SerializeObject(exception.Error);
+
+            Assert.Equal(expectedSerializedError, actualSerializedError);
+        }
+
+        [Theory]
+        [MemberData(nameof(Methods))]
+        public async Task ShouldHandleEmptyErrorResponse(Func<ITenantClient, TenantUpdateRequest, RequestOptions, Task<Tenant>> mut)
+        {
+            _fixture.SetupHandler(HttpStatusCode.Forbidden);
+
+            var exception = await Assert.ThrowsAsync<BasisTheoryException>(() => mut(_fixture.Client, new TenantUpdateRequest(), null));
+
+            Assert.Equal(403, exception.Error.Status);
+            Assert.Null(exception.Error.Title);
+            Assert.Null(exception.Error.Detail);
+        }
+
+        [Theory]
+        [MemberData(nameof(Methods))]
+        public async Task ShouldHandleNonBasisTheoryErrorResponse(Func<ITenantClient, TenantUpdateRequest, RequestOptions, Task<Tenant>> mut)
+        {
+            var error = Guid.NewGuid().ToString();
+
+            _fixture.SetupHandler(HttpStatusCode.InternalServerError, error);
+
+            var exception = await Assert.ThrowsAsync<BasisTheoryException>(() => mut(_fixture.Client, new TenantUpdateRequest(), null));
+
+            Assert.Equal(500, exception.Error.Status);
+            Assert.Null(exception.Error.Title);
+            Assert.Null(exception.Error.Detail);
+        }
 }
