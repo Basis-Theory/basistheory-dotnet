0c8477efd7a793fcd93bb2af157eda28c9f6fd66
Robert G. Greathouse <robert@basistheory.com>
2024-06-27 15:03:33 -0500

    csharp-netcore

--- scripts/export-templates.sh
+++ scripts/export-templates.sh
@@ -2,7 +2,7 @@
 
 docker pull openapitools/openapi-generator-cli:v7.6.0
 docker run --rm -v ${PWD}:/local openapitools/openapi-generator-cli:v6.2.1 author template \
-  -g csharp \
+  -g csharp-netcore \
   -o /local/.openapi-generator/templates \
 
 cd $(dirname $0)
--- scripts/generate-sdk.sh
+++ scripts/generate-sdk.sh
@@ -3,7 +3,7 @@
 docker pull openapitools/openapi-generator-cli:v7.6.0
 docker run --rm -v ${PWD}:/local openapitools/openapi-generator-cli:v6.2.1 generate \
   -i /local/swagger.json \
-  -g csharp \
+  -g csharp-netcore \
   -o /local \
   -t /local/.openapi-generator/templates \
   -c /local/openapi-config.yml \
