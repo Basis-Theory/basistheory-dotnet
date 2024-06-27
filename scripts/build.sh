083e96a3eb621e2643cb1b6ff2b54c2dbc4dce38
Robert G. Greathouse <robert@basistheory.com>
2024-06-27 13:31:54 -0500

    Re-add the previous build.sh

---
+++ scripts/build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e
+
+
+current_directory="$PWD"
+
+cd $(dirname $0)/../
+
+dotnet restore
+
+dotnet build BasisTheory.net.sln --no-restore -c Release
+
+result=$?
+
+cd "$current_directory"
+
+exit $result
